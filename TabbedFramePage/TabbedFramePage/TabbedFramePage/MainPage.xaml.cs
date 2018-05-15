e56ab20464a8a378152c503ff7b652b4ba94551a
JSCHWEIGERT <jschweigert@oemsolutions.net>
2018-05-15 13:58:00 -0600

    Added project

---
+++ TabbedFramePage/TabbedFramePage/TabbedFramePage/MainPage.xaml.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace TabbedFramePage
+{
+	public partial class MainPage : ContentPage
+	{
+        // The outline color for each tab
+        private Color OutlineColor = Color.LimeGreen;
+
+		public MainPage()
+		{
+			InitializeComponent();
+            InitializeElements();
+		}
+
+        private void InitializeElements()
+        {
+            
+        }
+
+        private void TabX_Frame_Tapped(object sender, EventArgs e)
+        {
+            /*
+             *  To Highlight A Tab
+             *  ------------------
+             *  - Turn all other tab frame border colors to transparent
+             *  - Turn all other tab background colors to transparent
+             *  - Turn selected tabs border color to desired color
+             *  - Turn selected tabs background color to desired color
+             *  
+             *  SelectedTab_Frame_Outline.BackgroundColor = Color.DesiredColor
+             *  SelectedTab_Frame_Outline.BorderColor = Color.DesiredColor
+             *  
+             *  OtherTab_Frame_Outline.BackgroundColor = Color.Transparent
+             *  OtherTab_Frame_Outline.BorderColor = Color.Transparent
+             *  
+             *  ...
+             */
+        }
+
+        private void Tab_Frame_Tapped(object sender, EventArgs e)
+        {
+            Frame SelectedFrame = sender as Frame;
+            SelectedFrame.BackgroundColor = OutlineColor;
+            SelectedFrame.BorderColor = OutlineColor;
+            
+            foreach(Frame item in TabHolder.Children.Where(x => x.GetType() == typeof(Frame)))
+            {
+                if(item.ClassId == "Outline" && item != SelectedFrame)
+                {
+                    item.BackgroundColor = Color.Transparent;
+                    item.BorderColor = Color.Transparent;
+                }
+            }   
+        }
+    }
+}
